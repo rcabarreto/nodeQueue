e11edc7504093e9a40193859cea88ddb78586fe2
Rodrigo Barreto <rcabarreto@gmail.com>
2019-04-12 15:55:07 -0300

    added license

--- src/server.js
+++ src/server.js
@@ -5,6 +5,30 @@
 import app from './app';
 import http from 'http';
 
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(port);
+server.on('listening', onListening);
+
+
+function normalizePort(val) {
+  const newPort = parseInt(val, 10);
+
+  if (isNaN(newPort)) {
+    return val;
+  }
+
+  if (newPort > 0) {
+    return newPort;
+  }
+
+  return false;
+}
+
+
+function onListening() {
+  console.log('Server running on port', port);
+}
